5eae1e2d1b9a69193e65c0def2ec5b880f1ac173
Pranay Nanda <pranaynanda@gmail.com>
2014-01-03 16:01:53 +0000

    Added regular expression to extract cost, description. Calculate & Display total spent

--- Expenses.py
+++ Expenses.py
@@ -1,5 +1,41 @@
 """Project - Expense report
 Feature List:
-1. """
+1. Categorize spending:
+	a. Groceries
+	b. Eating Out
+	c. Gas
+	d. Online Shopping
 
-print "Hello worlds, this is my project repo"
+2.  
+
+Bugs:
+1. Negative numbers are not deducted in the total
+2. Amazon(Remote+Calendar) format not matched"""
+import re
+
+def main():
+	
+	expense_file = open('Data.txt','r')
+	category_dict = {'Groceries': 0,
+					'Eating Out': 0, 
+					'Gas': 0, 
+					'Online Shopping': 0} #Dictionary that keeps track of spenidng in each category {category->sum}
+	total_spent = 0 #Total spent for the month
+
+
+	text_data = expense_file.read()
+	expense_file.close()
+	match = re.findall(r'(\w+\+*\s*\w+)\s+\-*\$([0-9]+\.[0-9]+)', text_data) #Re for all the items
+	#print match
+
+	for description, cost in match:
+		print description, float(cost)
+		total_spent += float(cost)
+
+
+	print total_spent
+
+
+
+if __name__ == '__main__':
+  main()
